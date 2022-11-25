8fc940f2644c10b849856f2c51f4085aeaa8ebac
m1w.ru <1@m1w.ru>
2022-11-26 02:03:46 +0300

    туть жопа :(

---
+++ 1.5/swiper.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper('.swiper', {
+        direction: 'horizontal',
+        loop: true,
+        pagination: {
+            el: '.swiper-pagination',
+        },
+        spaceBetween: 32,
+        //centeredSlides: true,
+        slidesPerView: 1,
+    });
